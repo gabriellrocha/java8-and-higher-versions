d6dcf4c6cd34e144b514957b03322f001c6d6322
gabriel <gabriell_rsilva@outlook.com>
2024-08-12 19:22:10 -0300

    feat: tipos primitivos

---
+++ src/com/gabriel/introducao/MyClass001TiposPrimitivos.java
@@ -0,0 +1,25 @@
+package com.gabriel.introducao;
+
+public class MyClass001TiposPrimitivos {
+    public static void main(String[] args) {
+
+        // são tipos de dados que a linguagem possui por padrão
+
+        // inteiros
+        byte typeByte = 1; // 8 bits intervalo -128 a 127
+        short typeShort = 32150; // 16 bits intervalo -32.768 a 32767
+        int typeInt = 15681201; // 32 bits intervalo de -2.147.483.648 a 2.147.483.647
+        long typeLong = 424151627; // 64 bits intervalo de -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+
+        // ponto flutuante
+        float typeFloat = 1.89f; // 32 bits aproximadamente 6 a 7 dígitos decimais de precisão
+        double typeDouble = 3.50; // 64 bits aproximadamente 15 a 16 dígitos decimais de precisão
+
+        // char
+        char typeChar = 'a'; // representa um caractere Unicode
+
+        // boolean
+        boolean typeBoolean = true; // recebe um valor lógico (true or false)
+
+    }
+}
