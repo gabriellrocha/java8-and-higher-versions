45f67bd46dffb8e7e1613521dae8fe2833b75c1b
gabriel <gabriell_rsilva@outlook.com>
2024-08-13 18:24:27 -0300

    feat: operadores relacionais

---
+++ src/com/gabriel/introducao/MyClass003OperadoresRelacionais.java
@@ -0,0 +1,20 @@
+package com.gabriel.introducao;
+
+public class MyClass003OperadoresRelacionais {
+    public static void main(String[] args) {
+
+        // usados para comparar dois valores
+        // SEMPRE retornam um valor boolean (true or false)
+        // < >  <= >= == !=
+        // menor, maior, menor ou igual, maior ou igual, igual, diferente
+
+        System.out.println( 10 > 20); // false
+        System.out.println(10 != 4); // true
+        System.out.println( 30 == 30); // true
+        System.out.println(53 <= 32); // false
+        System.out.println(21 >= 12); // true
+
+        // Lembre-se: Comparação de tipo composto (Objeto), não se usa esses operadores
+
+    }
+}
