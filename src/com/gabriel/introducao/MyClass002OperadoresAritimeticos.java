71a38967cbac24326eeed7d584efe67a9fe8192d
gabriel <gabriell_rsilva@outlook.com>
2024-08-13 18:22:28 -0300

    feat: operadores aritimeticos

---
+++ src/com/gabriel/introducao/MyClass002OperadoresAritimeticos.java
@@ -0,0 +1,27 @@
+package com.gabriel.introducao;
+
+public class MyClass002OperadoresAritimeticos {
+
+    public static void main(String[] args) {
+
+        // usados para realizar cálculos matemáticos
+        // + - * /
+        // % (resto divisão de um número pelo outro)
+        // ++ incrementa o valor da variável em 1
+        // -- decrementa o valor da variável em 1
+
+
+        /* Lembre-se: a divisão entre dois números inteiros resulta em um número inteiro,
+         truncando qualquer parte fracionária. */
+
+        int n1 = 10;
+        int n2 = 3;
+        System.out.println(n1/n2); // = 1
+
+        /* Para obter um resultado de ponto flutuante, pelo menos um dos operandos deve ser um número de ponto flutuante*/
+
+        double n3 = 3.0;
+        System.out.println(n1/n3); // = 3,33
+
+    }
+}
