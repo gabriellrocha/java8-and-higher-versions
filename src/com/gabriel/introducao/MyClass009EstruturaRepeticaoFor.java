800a16d919ec0dd0f33ddc3bfe8a68e85c290ecf
gabriel <gabriell_rsilva@outlook.com>
2024-08-14 17:04:38 -0300

    feat: estrutura repetição For

---
+++ src/com/gabriel/introducao/MyClass009EstruturaRepeticaoFor.java
@@ -0,0 +1,22 @@
+package com.gabriel.introducao;
+
+public class MyClass009EstruturaRepeticaoFor {
+    public static void main(String[] args) {
+
+        // Tem o mesmo objetivo do While, mas possui algumas particularidades
+        // Basicamente cada estrutura de repetição de adequa melhor a uma determinada situação
+
+        // Ideal quando você sabe de antemão quantas vezes o bloco de código deve ser executado
+
+
+//      Sintaxe: (inicializa variável local; condição; incremento)
+
+        for(int i = 0; i < 10; i++) { // contar de 0 - 9
+            System.out.println(i);
+        }
+
+        // for-each: Para iterar sobre elementos de arrays e coleções de maneira simplificada
+        // Estudo sobre o assunto mais a frente
+
+    }
+}
