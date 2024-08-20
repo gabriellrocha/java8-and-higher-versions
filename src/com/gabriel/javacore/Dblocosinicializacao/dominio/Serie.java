95d5bf0ec8a5398241bf4cf93ae7d2c91688f803
gabriel <gabriell_rsilva@outlook.com>
2024-08-20 18:41:20 -0300

    feat: bloco inicializacao static

--- src/com/gabriel/javacore/Dblocosinicializacao/dominio/Serie.java
+++ src/com/gabriel/javacore/Dblocosinicializacao/dominio/Serie.java
@@ -3,21 +3,25 @@ package com.gabriel.javacore.Dblocosinicializacao.dominio;
 public class Serie {
 
     private String nome;
-    private int[] episodios;
+    private static int[] episodios;
     private String diretor;
 
     // Bloco de inicialização de instância
     // É executado SEMPRE que uma nova instância é criada, antes do construtor da classe
 
-//    Ordem de execução (até o aprendido no momento)
+//    0 - Bloco static é inicializado quando a JVM carregar a classe
 //    1 - Alocado espaço na memória pro objeto
 //    2 - Cada atributo da classe é inicializado com os valores default ou valor que for definido
 //    3 - Bloco de inicialização de instância
 //    4 - Construtor
 
+    static {
+        System.out.println("Bloco static executado");
+        episodios = new int[10];
+    }
+
     {
         System.out.println("Bloco de inicialização de instância executado");
-        episodios = new int[10];
 
     }
 
@@ -40,5 +44,9 @@ public class Serie {
 
     public void setDiretor(String diretor) {
         this.diretor = diretor;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
     }
 }
