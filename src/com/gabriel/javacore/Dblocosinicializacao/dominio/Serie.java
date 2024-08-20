7b01f50a2afb241fbe18ff8039ec042e50f178bd
gabriel <gabriell_rsilva@outlook.com>
2024-08-20 18:21:13 -0300

    feat: bloco de inicializacao | ordem de execução pela JVM

---
+++ src/com/gabriel/javacore/Dblocosinicializacao/dominio/Serie.java
@@ -0,0 +1,44 @@
+package com.gabriel.javacore.Dblocosinicializacao.dominio;
+
+public class Serie {
+
+    private String nome;
+    private int[] episodios;
+    private String diretor;
+
+    // Bloco de inicialização de instância
+    // É executado SEMPRE que uma nova instância é criada, antes do construtor da classe
+
+//    Ordem de execução (até o aprendido no momento)
+//    1 - Alocado espaço na memória pro objeto
+//    2 - Cada atributo da classe é inicializado com os valores default ou valor que for definido
+//    3 - Bloco de inicialização de instância
+//    4 - Construtor
+
+    {
+        System.out.println("Bloco de inicialização de instância executado");
+        episodios = new int[10];
+
+    }
+
+    public Serie(String nome, String diretor) {
+        this.nome = nome;
+        this.diretor = diretor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+}
---
+++ src/com/gabriel/javacore/Dblocosinicializacao/testes/SerieTeste.java
@@ -0,0 +1,11 @@
+package com.gabriel.javacore.Dblocosinicializacao.testes;
+
+import com.gabriel.javacore.Dblocosinicializacao.dominio.Serie;
+
+public class SerieTeste {
+    public static void main(String[] args) {
+
+        Serie serie = new Serie("Supernatural", "Eric Kripke");
+
+    }
+}
