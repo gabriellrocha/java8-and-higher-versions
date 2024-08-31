dfc9f46d0bb2ddfd49ba015c63ae3bc2f06dde84
gabriel <gabriell_rsilva@outlook.com>
2024-08-31 17:48:13 -0300

    feat: modificador final para tipos referência

--- src/com/gabriel/javacore/Jmodificadorfinal/Carro.java
+++ src/com/gabriel/javacore/Jmodificadorfinal/Carro.java
@@ -7,6 +7,14 @@ public class Carro {
     public static final double VELOCIDADE_LIMITE = 250.0;
     public static final int QUANTIDADE_PORTAS;
 
+
+    // Atributo do tipo referência marcado como final, não pode "apontar" para outra referência, mas o Objeto em si
+    // pode alterar seu estado (valores internos)
+
+    // Ou seja, o comprador aqui não pode ser alterado
+    private final Comprador COMPRADOR = new Comprador("Gabriel");
+
+
     static {
         // É possível inicializar uma variável final desta forma, pois o Bloco 'static' é carregado antes dos atributos pela JVM
         QUANTIDADE_PORTAS = 4;
---
+++ src/com/gabriel/javacore/Jmodificadorfinal/Comprador.java
@@ -0,0 +1,18 @@
+package com.gabriel.javacore.Jmodificadorfinal;
+
+public class Comprador {
+
+    private String nome;
+
+    public Comprador(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
