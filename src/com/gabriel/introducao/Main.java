0d348f095460df8d9bde8c64015ea669bce836a3
gabriel <gabriell_rsilva@outlook.com>
2024-09-01 19:00:24 -0300

    docs: update readme

--- src/com/gabriel/introducao/Main.java
+++ src/com/gabriel/introducao/Main.java
@@ -1,6 +1,18 @@
 package com.gabriel.introducao;
 
 public class Main {
+
+    // isto é um comentário de uma linha
+
+    /*
+    * isto é um comentário de
+    * múltiplas linhas
+    * */
+
+    /**
+     * Isso é um comentário <strong>java doc</strong>
+     * */
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
